b12f6ca6ac84504f1ae1c3a8ee044a214153806c
teroqim <teroqim@gmail.com>
2014-10-07 10:40:06 -0700

    Rename to linked_list

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='linked_list',
+     version='0.1',
+     description='General purpose linked list',
+     url='https://github.com/teroqim/linkedlist',
+     author='Peter Andersson',
+     author_email='teroqim@gmail.com',
+     packages='linked_list',
+     zip_safe=False)
