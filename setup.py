84cb15874bb6af95b2887089f7f40e005c113c30
Kethan <kethan@vegunta.com>
2021-04-20 12:12:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='linkedlist',
      version='2.0.0',
      description='General purpose linked list',
+     long_description=long_description,
+     long_description_content_type="text/markdown",
      url='https://github.com/teroqim/linkedlist',
      author='Peter Andersson',
      author_email='teroqim@gmail.com',
      packages=['linkedlist'],
-     zip_safe=False)
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     zip_safe=False
+)
